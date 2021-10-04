d00a0a64ba6ca353e7b39b5ccb2ba5083c8a07b0
choc1403 <eloicx@gmail.com>
2021-10-03 19:36:52 -0600

    Agregando preguntas las primeras preguntas a la tabla Preguntas Predeterminadas

--- bdAdivinador/bdAdivinador.sql
+++ bdAdivinador/bdAdivinador.sql
@@ -54,23 +54,10 @@ CREATE TABLE IF NOT EXISTS Anfibios(
 );
 
 
--- DESC PreguntasPredeterminadas;
--- DESC Aves;
--- DESC Acuaticos;
--- DESC Mamiferos;
--- DESC Reptiles;
--- DESC Anfibios;
+DESC PreguntasPredeterminadas;
+DESC Aves;
+DESC Acuaticos;
+DESC Mamiferos;
+DESC Reptiles;
+DESC Anfibios;
 
--- INSERT INTO PreguntasPredeterminadas 
---    VALUES(1,'PUEDE VOLAR?',0,2,3),
---          (2,'PUEDE NADAR?',1,4,5),
---          (4,'ES UN MAMIFERO?',2,6,7),
---          (6,'ES UN REPTIL?',4,8,9),
---          (8,'ES UN ANFIBIO',6,0,10);
-
--- SELECT * FROM PreguntasPredeterminadas;
-
--- INSERT INTO PreguntasPredeterminadas
--- VARCHAR(1,'XD',0,2,3);
-
--- SELECT * FROM PreguntasPredeterminadas;
