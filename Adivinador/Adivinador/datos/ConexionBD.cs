401cd1cff218672da3e263867b331fd7c5f8024a
choc1403 <eloicx@gmail.com>
2021-10-06 01:13:02 -0600

    Se agrego el constructor de preguntas

--- Adivinador/Adivinador/datos/ConexionBD.cs
+++ Adivinador/Adivinador/datos/ConexionBD.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Adivinador.datos
 {
     class ConexionBD
     {
+        public MySqlConnection conexion()
+        {
+            String server, database, uid, pwd, info;
+            server = "localhost";
+            database = "bdAdivinador";
+            uid = "root";
+            pwd = "admin";
+
+            info = "Server = " + server + "; Database = " + database +
+                "; Uid = "+uid+"; Pwd = "+pwd;            
+
+            try
+            {
+                MySqlConnection conexion = new MySqlConnection(info);
+                Console.WriteLine("SE LOGRO LA CONEXION A LA BASE DE DATOS");
+                return conexion;
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("ERROR: " + e.Message);
+                return null;
+            }
+        }
     }
 }
---
+++ Adivinador/Adivinador/datos/ConstructorDePreguntas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivinador.datos
+{
+    class ConstructorDePreguntas
+    {
+        public int id { get; set; }
+        public String pregunta { get; set; }
+        public int nodoPadre { get; set; }
+        public int nodoIzquierdo { get; set; }
+        public int nodoDerecho { get; set }
+
+        public ConstructorDePreguntas()
+        {
+
+        }
+    }
+}
