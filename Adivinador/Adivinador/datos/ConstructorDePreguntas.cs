50957545cc377956aad98bee41f1b5659cb801df
choc1403 <eloicx@gmail.com>
2021-10-26 22:34:26 -0600

    Constructor de preguntas

---
+++ Adivinador/Adivinador/datos/ConstructorDePreguntas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivinador.datos
+{
+    class ConstructorDePreguntas
+    {
+        public int id { get; set; }
+        public String pregunta { get; set; }
+        
+        public ConstructorDePreguntas()
+        {
+
+        }
+    }
+}
